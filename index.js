a086cc9d0d7ac4708dda74702ae9268b1813965a
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2022-11-29 17:58:08 +0530

    Updated

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ function Showdata() {
     document.getElementById('card12').innerHTML=text
     document.getElementById('card13').innerHTML=text
     document.getElementById('card14').innerHTML=text
+    document.getElementById('card15').innerHTML=text
+    document.getElementById('card16').innerHTML=text
+    document.getElementById('card17').innerHTML=text
+    document.getElementById('card18').innerHTML=text
+    document.getElementById('card19').innerHTML=text
+    document.getElementById('card20').innerHTML=text
+    document.getElementById('card21').innerHTML=text
+    document.getElementById('card22').innerHTML=text
+    document.getElementById('card23').innerHTML=text
+    document.getElementById('card24').innerHTML=text
     console.log(text);
+}
 
+function Dyanamictext() {
+    document.getElementById('input').value= "Welcome"
 }

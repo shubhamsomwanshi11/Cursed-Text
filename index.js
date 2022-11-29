35c356b27960e10be498e9e4a1026c826d330ba8
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2022-11-29 17:08:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function Showdata() {
+    var text = document.getElementById('input').value
+    document.getElementById('card0').innerHTML=text
+    document.getElementById('card1').innerHTML=text
+    document.getElementById('card2').innerHTML=text
+    document.getElementById('card3').innerHTML=text
+    document.getElementById('card4').innerHTML=text
+    document.getElementById('card5').innerHTML=text
+    document.getElementById('card6').innerHTML=text
+    document.getElementById('card7').innerHTML=text
+    document.getElementById('card8').innerHTML=text
+    document.getElementById('card9').innerHTML=text
+    document.getElementById('card10').innerHTML=text
+    document.getElementById('card11').innerHTML=text
+    document.getElementById('card12').innerHTML=text
+    document.getElementById('card13').innerHTML=text
+    document.getElementById('card14').innerHTML=text
+    console.log(text);
+
+}
